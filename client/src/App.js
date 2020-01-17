10cb51e9095d92a4f9eeaf8d6bea993a490e440d
AbdelI <homeworkoracle@protonmail.com>
2020-01-17 11:15:52 +0100

    Added form to make a new project and add to the database

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,42 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
-
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [formInput, setFormInput] = useState({
+    name: undefined,
+    description: undefined
+  });
+  const onValueChange = event => {
+    setFormInput({ ...formInput, [event.target.name]: event.target.value });
+  };
   const [greeting, setGreeting] = useState("");
+  const [projects, setProjects] = useState([]);
+  // const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("/api/landing?name=cooldude")
+    // fetch("http://127.0.0.1:5100/api/landing?name=tom")
+    //   .then(res => res.json())
+    //   .then(greeting => setGreeting(greeting), console.log(greeting))
+    //   .catch(err => {
+    //     console.log("catch", err);
+    //   });
+
+    axios
+      .get("http://127.0.0.1:5100/api/projects")
+      // .then(res => res.json())
+      .then(projects => {
+        // console.log(projects);
+        setProjects(projects.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [projects]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5100/api/landing?name=tom")
       .then(res => res.json())
       .then(greeting => setGreeting(greeting), console.log(greeting))
       .catch(err => {
@@ -15,20 +44,99 @@ function App() {
       });
   }, []);
 
+  // fetch("http://127.0.0.1:5100/api/landing?name=tom")
+  //   .then(res => res.json())
+  //   .then(greeting => setGreeting(greeting), console.log(greeting))
+  //   .catch(err => {
+  //     console.log("catch", err);
+  //   });
+
+  const onHandleSubmit = (event, newProject) => {
+    event.preventDefault();
+
+    axios
+      .post("http://127.0.0.1:5100/api/projects", newProject)
+      .then(data => {
+        setFormInput({
+          name: "",
+          description: ""
+        });
+        // console.log(data);
+        // setProjects(projects);
+      })
+
+      .catch(err => console.log(err));
+  };
+  const DeleteUser = (event, id) => {
+    event.preventDefault();
+
+    axios
+      .delete(`http://127.0.0.1:5100/api/projects/${id}`)
+      .then(data => {
+        console.log(data);
+      })
+
+      .catch(err => err);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>hey {greeting}</p>
+
+        <form onSubmit={event => onHandleSubmit(event, formInput)}>
+          {/* <p>{formInput.name}</p>
+          <p>{formInput.description}</p> */}
+          <label>
+            Add a project you would like to work on <br />
+            <br />
+            <input
+              type="text"
+              placeholder="name of the project"
+              value={formInput.name}
+              name="name"
+              onChange={onValueChange}
+            />
+            <br />
+            <input
+              type="text"
+              placeholder="description goes here"
+              name="description"
+              value={formInput.description}
+              onChange={onValueChange}
+            />
+          </label>
+
+          <button type="submit">Submit your project</button>
+        </form>
+
+        {projects.map(proj => {
+          return (
+            <div
+              style={{
+                width: "50%",
+                height: "100%",
+                background: "grey",
+                margin: "20px",
+                borderRadius: "15px",
+                boxShadow: "10px 5px 5px pink"
+              }}
+              key={proj.id}
+            >
+              <h2 style={{ color: "black" }}>Name: {proj.name}</h2>
+              <p style={{ color: "black", fontWeight: "bold" }}>
+                Description: {proj.description}
+              </p>
+              <p>Id: {proj.id}</p>
+
+              <button onClick={event => DeleteUser(event, proj.id)}>
+                Delete this user
+              </button>
+            </div>
+          );
+        })}
       </header>
     </div>
   );
