f92d5eca7e0f85dd54e54fe3b783893c0da56dfd
AbdelI <homeworkoracle@protonmail.com>
2020-01-17 10:08:54 +0100

    App renders properly with welcome message

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+
+import "./App.css";
 
 function App() {
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    fetch("/api/landing?name=coldude")
+      .then(res => res.json())
+      .then(greeting => setGreeting(greeting), console.log(greeting))
+      .catch(err => {
+        console.log("catch", err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +28,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>hey {greeting}</p>
       </header>
     </div>
   );
